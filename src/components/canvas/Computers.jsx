9aec55a92faf70d7689d1d70703eee9c42992678
Julio Noguera <noguerajulio24@gmail.com>
2023-05-07 17:31:05 -0400

    update three.js

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
+import CanvasLoader from '../Loader.jsx';
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+    </mesh>
   )
 }
 
